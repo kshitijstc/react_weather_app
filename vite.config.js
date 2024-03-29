d9d1407508028182a1e903e7553252ce1eb03f35
kshitijstc <kshitijstc@gmail.com>
2024-03-29 14:08:05 +0530

    gitignore changed (dist commented) and vite.config.js changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react_weather_App",
+  base: "/react_weather_App/",
   plugins: [react()],
 })
